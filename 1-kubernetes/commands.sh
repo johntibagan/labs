1be3f4da9ae171ba98658aa774fd0820456d23dc
jtibagan <jtibagan@bancolombia.com.co>
2024-04-25 21:07:27 -0500

    feat: commands

--- 1-kubernetes/commands.sh
+++ 1-kubernetes/commands.sh
@@ -1,30 +1,23 @@
+# all
+kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/1-namespace.yaml
+# kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/#-FILE.yaml
+
+kubectl get OBJECT
+kubectl describe OBJECT
+
 # ---------- namespaces
 kubectl get namespaces
-
-kubectl apply -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/1-namespace.yaml
 kubectl get namespace john-dev #-o yaml
 kubectl describe namespace john-dev
 
-kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/1-namespace.yaml
-
 # ---------- pods
 kubectl get pods
-
-kubectl apply -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/2-pod.yaml
 kubectl describe pod parksmap-pod
 
-kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/2-pod.yaml
-
 # ---------- deployments
-
 kubectl get deployments
-
-kubectl apply -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/3-deployment.yaml
 kubectl describe deployment parksmap-deployment
-
 kubectl delete pod parksmap-deployment-<id-pod>
-
-kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/objects/3-deployment.yaml
 
 # ---- logs
 # un pod
@@ -32,7 +25,33 @@ kubectl logs parksmap-pod #-f
 # por un selector -l
 kubectl logs -l app=parksmap-deployment #-f
 
+# ---------- service
+kubectl get service
+
+# ---------- ingress
+kubectl get ingress
+kubectl get routes #solo en openshift
+
+# ---------- hpa - HorizontalPodAutoscaler
+kubectl get hpa
+
+# ---------- ConfigMap
+kubectl get configmap
+env | sort
+ls -la /home/appuser/config
+cat /home/appuser/config/file-1.json
+cat /home/appuser/config/file-2.xml
+
+# ---------- PersistentVolumeClaim
+kubectl get pvc
+echo "Contenido de un archivo temporal" > /tmp/data/file-temp.txt
+echo "Archivos permanentes en un PVC" > /mnt/data/file-pvc.txt
+cat /tmp/data/file-temp.txt
+cat /mnt/data/file-pvc.txt
+ls -la  /tmp/data/
+ls -la  /mnt/data/
+
 
 # --- ALL apply - remove
 kubectl apply -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/all.yaml
-kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/all.yaml
+# kubectl delete -f https://raw.githubusercontent.com/johntibagan/labs/main/1-kubernetes/all.yaml
